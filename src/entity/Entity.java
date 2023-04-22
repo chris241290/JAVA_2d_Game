3a6b6ee0898842adcbfe0d2c9f8a205f78bc2936
e-stanislao.davino <e-stanislao.davino@wolterskluwer.com>
2023-04-22 10:20:19 +0200

    Fix Formattazione + Added get/set in Entity + use super in Player

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -2,20 +2,141 @@ package entity;
 
 import java.awt.image.BufferedImage;
 
-public class Entity { // Classe padre per tutte le entità del gioco (npc,mostri,personaggio,etc.)
-	// queste variabili saranno presenti in tutte le entità del gioco che si muovono (mostri,npc etc)
-	// saranno ereditate da tutte le classi che fanno parte della superclasse Entity
-    // Variabile int per le coordinate sullo schermo
-	public int y,x;
-	// Variabile per la velocità
-	public int speed;
-	// BufferedImage descrive un immagine accessibile tramite una source
-	public BufferedImage up1,up2,down1,down2,left1,left2,right1,right2;
-	// Variabile stringa direction su cui faremo lo switch case delle stringhe
-	// "up" "down" etc. per gestire le variabili BufferedImagee aggiornare le animazioni su schermo
-	public String direction;
-	// variabili int contatori degli sprite per cambiare le animazioni
-	public int spriteCounter = 0;
-	public int spriteNum = 1;
-	
-	}
+// Classe padre per tutte le entità del gioco (npc,mostri,personaggio,etc.)
+public class Entity {
+
+  // queste variabili saranno presenti in tutte le entità del gioco che si muovono (mostri,npc etc)
+  // saranno ereditate da tutte le classi che fanno parte della superclasse Entity
+  // Variabile int per le coordinate sullo schermo
+  private int y;
+  private int x;
+  // Variabile per la velocità
+  private int speed;
+  // BufferedImage descrive un immagine accessibile tramite una source
+  private BufferedImage up1;
+  private BufferedImage up2;
+  private BufferedImage down1;
+  private BufferedImage down2;
+  private BufferedImage left1;
+  private BufferedImage left2;
+  private BufferedImage right1;
+  private BufferedImage right2;
+  // Variabile stringa direction su cui faremo lo switch case delle stringhe
+  // "up" "down" etc. per gestire le variabili BufferedImagee aggiornare le animazioni su schermo
+  private String direction;
+  // variabili int contatori degli sprite per cambiare le animazioni
+  private int spriteCounter = 0;
+  private int spriteNum = 1;
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
+  }
+
+  public BufferedImage getUp1() {
+    return up1;
+  }
+
+  public void setUp1(BufferedImage up1) {
+    this.up1 = up1;
+  }
+
+  public BufferedImage getUp2() {
+    return up2;
+  }
+
+  public void setUp2(BufferedImage up2) {
+    this.up2 = up2;
+  }
+
+  public BufferedImage getDown1() {
+    return down1;
+  }
+
+  public void setDown1(BufferedImage down1) {
+    this.down1 = down1;
+  }
+
+  public BufferedImage getDown2() {
+    return down2;
+  }
+
+  public void setDown2(BufferedImage down2) {
+    this.down2 = down2;
+  }
+
+  public BufferedImage getLeft1() {
+    return left1;
+  }
+
+  public void setLeft1(BufferedImage left1) {
+    this.left1 = left1;
+  }
+
+  public BufferedImage getLeft2() {
+    return left2;
+  }
+
+  public void setLeft2(BufferedImage left2) {
+    this.left2 = left2;
+  }
+
+  public BufferedImage getRight1() {
+    return right1;
+  }
+
+  public void setRight1(BufferedImage right1) {
+    this.right1 = right1;
+  }
+
+  public BufferedImage getRight2() {
+    return right2;
+  }
+
+  public void setRight2(BufferedImage right2) {
+    this.right2 = right2;
+  }
+
+  public String getDirection() {
+    return direction;
+  }
+
+  public void setDirection(String direction) {
+    this.direction = direction;
+  }
+
+  public int getSpriteCounter() {
+    return spriteCounter;
+  }
+
+  public void setSpriteCounter(int spriteCounter) {
+    this.spriteCounter = spriteCounter;
+  }
+
+  public int getSpriteNum() {
+    return spriteNum;
+  }
+
+  public void setSpriteNum(int spriteNum) {
+    this.spriteNum = spriteNum;
+  }
+}
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,139 +1,150 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
-import main.GamePanel;
 import main.KeyHandler;
 
-public class Player extends Entity { // classe Player (sottoclasse di Entity) che è la classe che definisce il nostro player (giocatore)
-	// Metodo costruttore di Player (argomenti : un oggetto di tipo GamePanel e uno di tipo KeyHandler, 
-	public Player(GamePanel gp, KeyHandler keiH) {
-		this.gp = gp;
-		this.keyH = keiH;
-		// chiamiamo il metoto setDefaultValues() all'interno di questo costruttore 
-		// per settare i valori di default
-		setDefaultValues();
-		getPlayerImage();
-	}
-	GamePanel gp;
-	KeyHandler keyH;
-	// Metodo che setta la posizione di default del Player
-	public void setDefaultValues() {
-		y = 100;
-		x = 100;
-		speed = 4;
-		direction = "down";
-	}
-	// Load delle immagini dalla source res/player sulle variabili delle direzioni
-	public void getPlayerImage() {
-		try {
-			up1 = ImageIO.read(getClass().getResourceAsStream("/player/up1.png"));
-			up2 = ImageIO.read(getClass().getResourceAsStream("/player/up2.png"));
-			down1 = ImageIO.read(getClass().getResourceAsStream("/player/down2.png"));
-			down2 = ImageIO.read(getClass().getResourceAsStream("/player/down1.png"));
-			left1 = ImageIO.read(getClass().getResourceAsStream("/player/left1.png"));
-			left2 = ImageIO.read(getClass().getResourceAsStream("/player/left2.png"));
-			right1 = ImageIO.read(getClass().getResourceAsStream("/player/right1.png"));
-			right2 =ImageIO.read(getClass().getResourceAsStream("/player/right2.png"));
+// classe Player (sottoclasse di Entity) che è la classe che definisce il nostro player (giocatore)
+public class Player extends Entity {
 
-		}catch(IOException e){
-			e.printStackTrace();
-		};
-	}
+  private int tileSize;
+  private KeyHandler keyH;
 
-	public void update() {
-		if(keyH.upPressed==true || keyH.downPressed==true || keyH.leftPressed==true || keyH.rightPressed==true) {
-			// Struttura condizionale che a seconda del tasto premuto updata la posizione del tiles del player
-			// Sè la variabile upPressed(sopra) è settata a true (quindi è stato premuto il tasto associato a playerup(w nel nostro caso)
-			// allora fa l'operazione playerY = playerY - playerSpeed
-			if(keyH.upPressed == true) {
-				// setta direction nella direzione corrispondente 
-				direction = "up";
-				// sottrae dalle coordinate Y la velocità impostata del player per farlo muovere
-				y -= speed;
+  // Metodo costruttore di Player (argomenti : un oggetto di tipo GamePanel e uno di tipo KeyHandler,
+  public Player(int tileSize, KeyHandler keyH) {
+    this.tileSize = tileSize;
+    this.keyH = keyH;
+    // chiamiamo il metoto setDefaultValues() all'interno di questo costruttore
+    // per settare i valori di default
+    setDefaultValues();
+    getPlayerImage();
+  }
 
-			}
-			// Fa l'opposto (addizione) per farlo andare giù
-			else if(keyH.downPressed == true) {
-				direction = "down";
-				y += speed;
-			}
-			// Fa le stesse operazioni sulla variabile PlayerX per muoversi a destra e sinistra 
-			else if(keyH.leftPressed == true) {
-				direction = "left";
-				x -=speed;
-			}
+  // Metodo che setta la posizione di default del Player
+  public void setDefaultValues() {
+    super.setY(100);
+    super.setX(100);
+    super.setSpeed(4);
+    super.setDirection("down");
+  }
 
-			else if(keyH.rightPressed == true) {
-				direction = "right";
-				x += speed;
-			}
-			spriteCounter++;
-			if(spriteCounter > 10) {
-				if(spriteNum == 1) {
-					spriteNum = 2;
-				}
-				else if(spriteNum ==2) {
-					spriteNum=1;
-				}
-				spriteCounter=0;
-			}
-		}
+  // Load delle immagini dalla source res/player sulle variabili delle direzioni
+  public void getPlayerImage() {
+    try {
+      super.setUp1(ImageIO.read(getClass().getResourceAsStream("/player/up1.png")));
+      super.setUp2(ImageIO.read(getClass().getResourceAsStream("/player/up2.png")));
+      super.setDown1(ImageIO.read(getClass().getResourceAsStream("/player/down2.png")));
+      super.setDown2(ImageIO.read(getClass().getResourceAsStream("/player/down1.png")));
+      super.setLeft1(ImageIO.read(getClass().getResourceAsStream("/player/left1.png")));
+      super.setLeft2(ImageIO.read(getClass().getResourceAsStream("/player/left2.png")));
+      super.setRight1(ImageIO.read(getClass().getResourceAsStream("/player/right1.png")));
+      super.setRight2(ImageIO.read(getClass().getResourceAsStream("/player/right2.png")));
 
-	}
-	public void draw(Graphics g2) {
-		// Metodo che setta colore utilizzato per disegnare oggetti
-		//g2.setColor(Color.white);
-		// Metodo che disegna un rettangolo (tra parentesi le dimensioni) possiamo cambiarle tramite le variabili utilizzate
-		// con l'update saranno aggiornate le variabili dell'argomento fillRect e con il redraw sarà ridisegnato tutto con le coordinate aggiornate
-		//g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-		BufferedImage image=null;
-		switch(direction) {
-		// Switch case sulla variabile BufferedImage direction con i case stringa
-		// daranno il valore (stringa/directory dell'immagine) alla variabile image
-		// gli if sfruttano la variabile int come contatore per cambiare le animazioni
-		case "up":
-			if (spriteNum == 1) {
-				image=up1;
-			}
-			if (spriteNum == 2) {
-				image=up2;
-			}
-			break;
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-		case "down":
-			if(spriteNum ==1) {
-				image=down1;
-			}
-			if(spriteNum == 2) {
-				image=down2;
-			}
-			break;
+  public void update() {
+    if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
+      // Struttura condizionale che a seconda del tasto premuto updata la posizione del tiles del player
+      // Sè la variabile upPressed(sopra) è settata a true (quindi è stato premuto il tasto associato a playerup(w nel nostro caso)
+      // allora fa l'operazione playerY = playerY - playerSpeed
+      if (keyH.upPressed == true) {
+        System.out.println("Premuto tasto UP");
+        // setta direction nella direzione corrispondente
+        super.setDirection("up");
+        // sottrae dalle coordinate Y la velocità impostata del player per farlo muovere
+        super.setY(super.getY() - super.getSpeed());
+      }
+      // Fa l'opposto (addizione) per farlo andare giù
+      else if (keyH.downPressed == true) {
+        System.out.println("Premuto tasto DOWN");
+        super.setDirection("down");
+        super.setY(super.getY() + super.getSpeed());
+      }
+      // Fa le stesse operazioni sulla variabile PlayerX per muoversi a destra e sinistra
+      else if (keyH.leftPressed == true) {
+        System.out.println("Premuto tasto LEFT");
+        super.setDirection("left");
+        super.setX(super.getX() - super.getSpeed());
+      } else if (keyH.rightPressed == true) {
+        System.out.println("Premuto tasto RIGHT");
+        super.setDirection("right");
+        super.setX(super.getX() + super.getSpeed());
+      }
+      super.setSpriteCounter(super.getSpriteCounter() + 1);
+      if (super.getSpriteCounter() > 10) {
+        if (super.getSpriteNum() == 1) {
+          super.setSpriteNum(2);
+        } else if (super.getSpriteNum() == 2) {
+          super.setSpriteNum(1);
+        }
+        super.setSpriteCounter(0);
+      }
+    }
 
-		case "left":
-			if(spriteNum ==1) {
-				image=left1;
-			}
-			if(spriteNum == 2) {
-				image=left2;
-			}
-			break;
+  }
 
-		case "right":
-			if(spriteNum ==1) {
-				image=right1;
-			}
-			if(spriteNum == 2) {
-				image=right2;
-			}
+  public void draw(Graphics g2) {
+    // Metodo che setta colore utilizzato per disegnare oggetti
+    //g2.setColor(Color.white);
+    // Metodo che disegna un rettangolo (tra parentesi le dimensioni) possiamo cambiarle tramite le variabili utilizzate
+    // con l'update saranno aggiornate le variabili dell'argomento fillRect e con il redraw sarà ridisegnato tutto con le
+    // coordinate aggiornate
+    //g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+    BufferedImage image = null;
+    switch (super.getDirection()) {
+      // Switch case sulla variabile BufferedImage direction con i case stringa
+      // daranno il valore (stringa/directory dell'immagine) alla variabile image
+      // gli if sfruttano la variabile int come contatore per cambiare le animazioni
+      case "up":
+        if (super.getSpriteNum() == 1) {
+          image = super.getUp1();
+          System.out.println("Draw immagine Up1");
+        }
+        if (super.getSpriteNum() == 2) {
+          image = super.getUp2();
+          System.out.println("Draw immagine Up2");
+        }
+        break;
 
-			break;
-		}
-		g2.drawImage(image, x, y,gp.tileSize,gp.tileSize,null);
-	}
+      case "down":
+        if (super.getSpriteNum() == 1) {
+          image = super.getDown1();
+          System.out.println("Draw immagine Down1");
+        }
+        if (super.getSpriteNum() == 2) {
+          image = super.getDown2();
+          System.out.println("Draw immagine Down2");
+        }
+        break;
+
+      case "left":
+        if (super.getSpriteNum() == 1) {
+          image = super.getLeft1();
+          System.out.println("Draw immagine Left1");
+        }
+        if (super.getSpriteNum() == 2) {
+          image = super.getLeft2();
+          System.out.println("Draw immagine Left2");
+        }
+        break;
+
+      case "right":
+        if (super.getSpriteNum() == 1) {
+          image = super.getRight1();
+          System.out.println("Draw immagine Right1");
+        }
+        if (super.getSpriteNum() == 2) {
+          image = super.getRight2();
+          System.out.println("Draw immagine Right2");
+        }
+
+        break;
+    }
+    g2.drawImage(image, super.getX(), super.getY(), tileSize, tileSize, null);
+  }
 }
